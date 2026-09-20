b82b63485f7dfee9c06ae6c70c8e3c4e33b14875
JamesDirr1 <dirrj2@mymail.nku.edu>
2026-09-19 21:28:29 -0400

    chore: init todo solution

---
+++ src/Todo.Cli/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.CommandLine;
+
+RootCommand rootCommand = new RootCommand("A simple todo application");
+
+Command statusCommand = new Command("status", "Shows the current todo status");
+
+statusCommand.SetAction(parseResult =>
+{
+    Console.WriteLine("Todo is ready.");
+});
+
+rootCommand.Subcommands.Add(statusCommand);
+
+return rootCommand.Parse(args).Invoke();
